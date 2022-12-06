57052b7bc9c5c48b1918942202aa96c4588409b7
Giuliano <giuliano.decesares@globant.com>
2022-12-06 11:16:37 -0300

    Added testing for client and server

---
+++ commvent_test.go
@@ -0,0 +1,104 @@
+package commvent_test
+
+import (
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/GiulianoDecesares/commvent/client"
+	"github.com/GiulianoDecesares/commvent/primitives"
+	"github.com/GiulianoDecesares/commvent/server"
+)
+
+const (
+	testingKey   = "TESTING_KEY"
+	testingValue = "TESTING_STRING"
+)
+
+var (
+	address = url.URL{
+		Host: ":5555",
+	}
+
+	sendEvents chan bool = make(chan bool)
+
+	testingCommand *primitives.Command = primitives.NewCommand("TESTING_COMMAND")
+	testingEvent   *primitives.Event   = primitives.NewEvent("TESTING_EVENT")
+
+	currentSever  *server.Server = server.NewServer(address)
+	currentClient *server.Client
+
+	eventCounter int = 0
+)
+
+func updateEventCounter(event *primitives.Event, context *testing.T) {
+	if event != nil && event.Type == testingEvent.Type {
+		eventCounter++
+		context.Logf("Event counter: %d", eventCounter)
+	}
+}
+
+func onNewServerClient(client *server.Client, context *testing.T) {
+	context.Logf("[Server] Client connected. Regsitering")
+	currentClient = client
+
+	currentClient.HandleDisconnect(func(disconnectionState error) {
+		context.Log("[Server] Client disconnected")
+		currentClient = nil
+	})
+
+	currentClient.HandleEvents(func(event *primitives.Event) {
+		context.Log("[Server] Event received")
+		updateEventCounter(event, context)
+	})
+
+	client.SendCommand(testingCommand)
+}
+
+func clientCommandHandler(command *primitives.Command, context *testing.T) {
+	context.Log("[Client] Command received")
+
+	if command.Type != testingCommand.Type {
+		context.Errorf("[Client] Command type should be %s but is %s", testingCommand.Type, command.Type)
+	}
+
+	if !command.Arguments.HasKey(testingKey) {
+		context.Errorf("[Client] Received command should have %s key", testingKey)
+	} else if command.Arguments.GetString(testingKey, "") != testingValue {
+		context.Errorf("[Client] Received command should have %s key with value %s", testingKey, testingValue)
+	} else {
+		sendEvents <- true
+	}
+}
+
+func Test(context *testing.T) {
+	testingCommand.Arguments.SetString(testingKey, testingValue)
+
+	go currentSever.Listen(func(client *server.Client) {
+		context.Log("[Server] Server listening")
+		onNewServerClient(client, context)
+	})
+
+	client := client.NewClient(func(command *primitives.Command) {
+		clientCommandHandler(command, context)
+	})
+
+	if err := client.Begin(address); err != nil {
+		context.Fatalf("Error while starting client: %s", err.Error())
+	}
+
+	defer client.Stop()
+
+	if <-sendEvents {
+		for index := 0; index < 10; index++ {
+			context.Log("[Client] Sending testing event")
+
+			client.SendEvent(testingEvent)
+			time.Sleep(time.Second)
+		}
+
+		if eventCounter != 10 {
+			context.Errorf("Amount of received events after sending 10 is %d and should be %d", eventCounter, 10)
+		}
+	}
+}
